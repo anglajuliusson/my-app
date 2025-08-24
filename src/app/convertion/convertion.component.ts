7f2f0e82eeabd3939dd846a8de13ae6468653ed9
Ängla Juliusson <angla02@icloud.com>
2025-08-24 17:19:11 +0200

    Skapa två undersidor

---
+++ src/app/convertion/convertion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertion',
+  imports: [],
+  templateUrl: './convertion.component.html',
+  styleUrl: './convertion.component.css'
+})
+export class ConvertionComponent {
+
+}
