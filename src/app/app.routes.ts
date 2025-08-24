3e7212c36ffec3d68f2d3e6e78d2f2c3814f976f
Ängla Juliusson <angla02@icloud.com>
2025-08-24 17:37:16 +0200

    Inkludera komponenter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Home/home.component';
+import { ConvertionComponent } from './Convertion/convertion.component';
+import { DescriptionComponent } from './Description/description.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'convertion', component: ConvertionComponent },
+    { path: 'description', component: DescriptionComponent }
+];
