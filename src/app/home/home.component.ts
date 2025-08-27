885a1149ff4af7ad2958f53e539545c72cfc25f0
Ängla Juliusson <angla02@icloud.com>
2025-08-27 09:59:55 +0200

    Implementera de nya bilderna på startsidan

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   portrattImage: string = "/images/portratt.png";
+  boatImage: string = "/images/boat.png";
+  flowersImage: string = "/images/flowers.png";
+  houseImage: string = "/images/house.png";
+  ozzyImage: string = "/images/ozzy.png";
+  sunsetImage: string = "/images/sunset.png";
+  raceImage: string = "/images/race.png";
 }
